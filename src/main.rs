02b0b75d263206e342282b0a03b3fd90af4f8afc
BrianReischl <breischl@gmail.com>
2021-12-08 08:38:04 -0700

    Finished Day1, Problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 mod depth_diffs;
+use depth_diffs::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
-    let depths = vec![10, 11, 9, 8, 23, 24, 24];
-    println!("Changes: {:?}", depth_diffs::calculate_diffs(&depths));
-    println!(
-        "Number of increases: {}",
-        depth_diffs::calculate_increase_count(&depths)
-    );
+    let path = Path::new("inputs/day1.txt");
+    let path_display = path.display();
+    println!("Reading input from {}", path_display);
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path_display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", path_display, why),
+        Ok(_) => println!("Successfully read {} characters", s.len()),
+    }
+
+    let depths: Vec<u16> = s
+        .lines()
+        .map(|line| match line.parse() {
+            Err(why) => panic!("Failed to parse {} to integer because {}", line, why),
+            Ok(num) => num,
+        })
+        .collect();
+    println!("Depths: {:?}", &depths);
+    println!("Changes: {:?}", calculate_diffs(&depths));
+    println!("Number of increases: {}", calculate_increase_count(&depths));
 }
