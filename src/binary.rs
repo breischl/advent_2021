7d73c7cdb6106d70726dacde0255e2cbfb0c7345
BrianReischl <breischl@gmail.com>
2021-12-09 20:15:06 -0700

    Little bit less magic numberness

--- src/binary.rs
+++ src/binary.rs
@@ -1,5 +1,8 @@
 //A very bit-twiddly answer to Day 3, Part 1
 //It was fun mucking around with bits, but I suspect this is a wildly verbose and inefficient solution
+const BIT_LENGTH: usize = 12; //all the inputs are actually 12 bits long
+const NUM_MASK: u16 = 0x0FFF; //mask a u16 down to 12 bits
+
 pub fn run(input: String) -> Result<String, String> {
     let binary_lines: Vec<u16> = input
         .lines()
@@ -12,7 +15,7 @@ pub fn run(input: String) -> Result<String, String> {
     println!("Most common bits: {:?}", most_common_bits);
 
     let gamma = bits_to_int(&most_common_bits);
-    let epsilon = (!gamma & 0b0000111111111111);
+    let epsilon = !gamma & NUM_MASK;
     let power = gamma as u64 * epsilon as u64;
     Ok(String::from(format!(
         "Gamma: {}, Epsilon: {}, Power: {}",
@@ -20,21 +23,21 @@ pub fn run(input: String) -> Result<String, String> {
     )))
 }
 
-fn bits_to_int(bits: &[u16; 16]) -> u16 {
+fn bits_to_int(bits: &[u16; BIT_LENGTH]) -> u16 {
     let mut int = 0u16;
-    for i in 0..16 {
-        let shifted = bits[i].clone() << (15 - i);
+    for i in 0..BIT_LENGTH {
+        let shifted = bits[i].clone() << (BIT_LENGTH - i - 1);
         int = int | shifted;
     }
     int
 }
 
-fn find_most_common_bits(ints: &Vec<u16>) -> [u16; 16] {
-    let mut counts: [u16; 16] = [0; 16];
+fn find_most_common_bits(ints: &Vec<u16>) -> [u16; BIT_LENGTH] {
+    let mut counts: [u16; BIT_LENGTH] = [0; BIT_LENGTH];
 
     for int in ints {
         for i in 0..counts.len() as u16 {
-            counts[i as usize] += extract_bit(*int, 15 - i);
+            counts[i as usize] += extract_bit(*int, BIT_LENGTH as u16 - i - 1);
         }
     }
 
@@ -53,7 +56,7 @@ fn find_most_common_bits(ints: &Vec<u16>) -> [u16; 16] {
 }
 
 fn extract_bit(num: u16, pos: u16) -> u16 {
-    (num >> pos) & 0b0000000000000001
+    (num >> pos) & 0x0001
 }
 
 fn parse_binary(s: &str) -> Result<u16, String> {
@@ -67,10 +70,10 @@ mod test {
 
     #[test]
     pub fn parse_binary_works() {
-        assert_eq!(Ok(1u16), parse_binary("0000000000000001"));
-        assert_eq!(Ok(32769u16), parse_binary("1000000000000001"));
-        assert_eq!(Ok(128u16), parse_binary("0000000010000000"));
-        assert_eq!(Ok(131u16), parse_binary("0000000010000011"));
+        assert_eq!(Ok(1u16), parse_binary("000000000001"));
+        assert_eq!(Ok(2049u16), parse_binary("100000000001"));
+        assert_eq!(Ok(128u16), parse_binary("000010000000"));
+        assert_eq!(Ok(131u16), parse_binary("000010000011"));
     }
 
     #[test]
@@ -88,12 +91,12 @@ mod test {
     #[test]
     pub fn find_mcb_works() {
         let input = vec![
-            parse_binary("0000000000000001").unwrap(),
-            parse_binary("1000000000000001").unwrap(),
-            parse_binary("0000000010000000").unwrap(),
-            parse_binary("0000000010000011").unwrap(),
+            parse_binary("000000000001").unwrap(),
+            parse_binary("100000000001").unwrap(),
+            parse_binary("000010000000").unwrap(),
+            parse_binary("000010000011").unwrap(),
         ];
-        let expected: Vec<u16> = "0000000010000001"
+        let expected: Vec<u16> = "000010000001"
             .chars()
             .map(|c| c.to_string().parse::<u16>().unwrap())
             .collect();
@@ -102,28 +105,13 @@ mod test {
 
     #[test]
     pub fn bits_to_int_works() {
-        assert_eq!(
-            49153,
-            bits_to_int(&[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        );
-        assert_eq!(
-            0,
-            bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        );
+        assert_eq!(3073, bits_to_int(&[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]));
+        assert_eq!(0, bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
 
-        assert_eq!(
-            1,
-            bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        );
+        assert_eq!(1, bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]));
 
-        assert_eq!(
-            9,
-            bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1])
-        );
+        assert_eq!(9, bits_to_int(&[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1]));
 
-        assert_eq!(
-            1033,
-            bits_to_int(&[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1])
-        );
+        assert_eq!(1033, bits_to_int(&[0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1]));
     }
 }
