fecb4562e1886a1252c8579ba582b8df81ad69af
BrianReischl <breischl@gmail.com>
2021-12-14 20:22:55 -0700

    Working with test input

--- src/seven_segment.rs
+++ src/seven_segment.rs
@@ -1,3 +1,26 @@
 pub fn run(input: String) -> Result<String, String> {
-    unimplemented!();
+    let out_digits_count = input
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split('|').map(|s| s.trim()).collect();
+            //println!("parts={:?}", parts);
+            let inputs: Vec<&str> = parts[0].split_whitespace().collect();
+            let outputs: Vec<&str> = parts[1].split_whitespace().collect();
+            (inputs, outputs)
+        })
+        .map(|(_inputs, outputs)| {
+            outputs
+                .iter()
+                .filter(|&digit| match digit.len() {
+                    2 | 3 | 4 | 7 => {
+                        //println!("matching {}", digit);
+                        true
+                    }
+                    _ => false,
+                })
+                .count()
+        })
+        .sum::<usize>();
+
+    Ok(format!("out_digits={}", out_digits_count))
 }
